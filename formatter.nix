2564b9046860b40bbb96d1a4a71d775d06303665
Notarin Steele <424c414e4b@gmail.com>
2026-06-27 23:53:41 -0400

    Set up a canonical formatter
    Formatters are: csharpier, alejandra, mdformat, deadnix, statix, and yamlfmt.

---
+++ formatter.nix
@@ -0,0 +1,61 @@
+{
+  treefmt-nix ?
+    import (builtins.fetchGit {
+      url = "https://github.com/numtide/treefmt-nix";
+      rev = "128222dc911b8e2e18939537bed1762b7f3a04aa";
+    }),
+  checkDir ? null,
+  lib,
+  writeShellScriptBin,
+  formats,
+  treefmt,
+  csharpier,
+  alejandra,
+  deadnix,
+  mdformat,
+  statix,
+  runCommandLocal,
+  yamlfmt,
+}: let
+  treefmt-pkgs = let
+    # Restructure `python3Packages` exactly how treefmt-nix wants it
+    python3Packages = {inherit mdformat;};
+  in {
+    inherit
+      lib
+      writeShellScriptBin
+      treefmt
+      formats
+      csharpier
+      alejandra
+      deadnix
+      python3Packages
+      statix
+      runCommandLocal
+      yamlfmt
+      ;
+  };
+  treefmt-settings = {
+    settings = {
+      allow-missing-formatter = false;
+    };
+    programs = {
+      csharpier.enable = true;
+      alejandra.enable = true;
+      mdformat.enable = true;
+      deadnix.enable = true;
+      statix.enable = true;
+      yamlfmt = {
+        enable = true;
+        settings.formatter = {
+          indentless_arrays = true;
+          retain_line_breaks_single = true;
+        };
+      };
+    };
+  };
+  output = treefmt-nix.evalModule treefmt-pkgs treefmt-settings;
+in
+  if checkDir == null
+  then output.config.build.wrapper
+  else output.config.build.check checkDir
--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
-{ pkgs ? (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in import (builtins.fetchTarball {
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
-  sha256 = lock.nodes.nixpkgs.locked.narHash;
-}) { }) }:
-
-let
+{
+  pkgs ? (let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+      sha256 = lock.nodes.nixpkgs.locked.narHash;
+    }) {}),
+}: let
   dependencies = with pkgs; [
     dotnet-sdk_10
     icu
@@ -46,14 +47,15 @@ let
     cups
     python3
   ];
-in pkgs.mkShell {
-  name = "space-station-14-devshell";
-  buildInputs = [ pkgs.gtk3 ];
-  packages = dependencies;
-  shellHook = ''
-    export GLIBC_TUNABLES=glibc.rtld.dynamic_sort=1
-    export ROBUST_SOUNDFONT_OVERRIDE=${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2
-    export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath dependencies}
-  '';
-}
+in
+  pkgs.mkShell {
+    name = "space-station-14-devshell";
+    buildInputs = [pkgs.gtk3];
+    packages = dependencies;
+    shellHook = ''
+      export GLIBC_TUNABLES=glibc.rtld.dynamic_sort=1
+      export ROBUST_SOUNDFONT_OVERRIDE=${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2
+      export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath dependencies}
+    '';
+  }

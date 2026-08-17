79242412c6163b1ec4ed688b3e4cbc50ba38b635
Notarin Steele <424c414e4b@gmail.com>
2026-08-17 02:47:36 -0400

    Removed ban strictness
    Savebans are now all considered totalbans.
    This will likely be reverted when an application is available.
    The reason for the removal is it is currently unused, and is
    debatably technical debt. Removing it increases legibility,
    while also preserving an easy to revert diff where should an
    application be found, can be reverted and given a use, and
    also allows for the easy option for a different approach to be
    taken should another path be decided on.

--- Content.Server/_HL/RoundPersistence/SaveBans/SaveBanStore.cs
+++ Content.Server/_HL/RoundPersistence/SaveBans/SaveBanStore.cs
@@ -23,39 +23,16 @@ public static partial class SaveBanStore
     }
 
     /// <summary>
-    /// This type represents the strictness of the ban on the said item.
-    /// The strictness represents what course of action will be taken on an item should someone try to save it.
-    /// </summary>
-    public abstract record SaveRestrictionStrictness
-    {
-        /// <summary>
-        /// The strictest ban possible. If you try to save this item, it will be deleted, and unrecoverable.
-        /// </summary>
-        public sealed record TotalBan : SaveRestrictionStrictness;
-        /// <summary>
-        /// A lighter form of restriction.
-        /// When an item with this strictness is saved, during the check, its associated handler will be executed on the item.
-        /// This will usually mean wiping or reverting some data stored on the said item.
-        /// </summary>
-        /// <param name="Handle">A function that takes an entity manager, and the entity uid, and performs an arbitrary action.</param>
-        public abstract record PartialBan(Action<EntityManager, EntityUid> Handle) : SaveRestrictionStrictness;
-    }
-
-    /// <summary>
     /// This is the type for the actual representation of items that are officially restricted from saving in some form or another.
     /// Also contains some factory methods to make adding new banned items simplified.
     /// </summary>
     /// <param name="BannedFlag"><see cref="Content.Server._HL.RoundPersistence.SaveBans.SaveBanStore.SaveBanFlag"/></param>
     /// <param name="Reason">A serialized formal explanation of why we cannot allow players to save this item.</param>
-    /// <param name="Strictness"><see cref="Content.Server._HL.RoundPersistence.SaveBans.SaveBanStore.SaveRestrictionStrictness"/></param>
     public sealed record SaveBan(
         SaveBanFlag BannedFlag,
-        string Reason,
-        SaveRestrictionStrictness Strictness)
+        string Reason)
     {
-        public static SaveBan Entity(string prototype, string reason, SaveRestrictionStrictness strictness) =>
-            new(new SaveBanFlag.SaveBanFlagByEntity(prototype), reason, strictness);
-        public static SaveBan Component(string prototype, string reason, SaveRestrictionStrictness strictness) =>
-            new(new SaveBanFlag.SaveBanFlagByComponent(prototype), reason, strictness);
+        public static SaveBan Entity(string prototype, string reason) => new(new SaveBanFlag.SaveBanFlagByEntity(prototype), reason);
+        public static SaveBan Component(string prototype, string reason) => new(new SaveBanFlag.SaveBanFlagByComponent(prototype), reason);
     }
 }
